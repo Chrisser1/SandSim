a7613602784919faba3c38de22f21f0596472528
Chrisser1 <chrisgthomsen0310@gmail.com>
2024-02-01 17:52:14 +0100

    It's on the gpu using vulkano and lot of other stuff

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,239 @@
-use app::MyApp;
-mod app;
+mod ca_simulator;
+mod camera;
+mod gui;
+mod quad_pipeline;
+mod render;
+mod utils;
+mod vertex;
+mod matter;
 
-fn main() -> Result<(), eframe::Error> {
-    let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([1000.0, 1000.0]),
-        ..Default::default()
+use bevy::{
+    input::mouse::MouseWheel,
+    prelude::*,
+    window::{close_on_esc, WindowMode},
+    time::FixedTimestep,
+};
+use bevy_vulkano::{BevyVulkanoWindows, VulkanoWinitConfig, VulkanoWinitPlugin};
+
+use crate::{
+    ca_simulator::CASimulator,
+    camera::OrthographicCamera,
+    gui::user_interface,
+    render::FillScreenRenderPass,
+    utils::{cursor_to_world, get_canvas_line, MousePos},
+    matter::MatterId,
+};
+
+pub const WIDTH: f32 = 1920.0;
+pub const HEIGHT: f32 = 1000.0;
+pub const CANVAS_SIZE_X: u32 = 512;
+pub const CANVAS_SIZE_Y: u32 = 512;
+pub const LOCAL_SIZE_X: u32 = 32;
+pub const LOCAL_SIZE_Y: u32 = 32;
+pub const NUM_WORK_GROUPS_X: u32 = CANVAS_SIZE_X / LOCAL_SIZE_X;
+pub const NUM_WORK_GROUPS_Y: u32 = CANVAS_SIZE_Y / LOCAL_SIZE_Y;
+pub const SIM_FPS: f64 = 60.0;
+/// Grey scale theme for cool looks
+pub const CLEAR_COLOR: [f32; 4] = [0.0; 4];
+pub const EMPTY_COLOR: u32 = 0x0;
+pub const CAMERA_MOVE_SPEED: f32 = 200.0;
+
+pub struct DynamicSettings {
+    pub brush_radius: f32,
+    pub draw_matter: MatterId,
+    pub is_paused: bool,
+}
+
+impl Default for DynamicSettings {
+    fn default() -> Self {
+        Self {
+            brush_radius: 4.0,
+            draw_matter: MatterId::Sand,
+            is_paused: false,
+        }
+    }
+}
+
+fn main() {
+    App::new()
+        .insert_non_send_resource(VulkanoWinitConfig::default())
+        .insert_resource(WindowDescriptor {
+            width: WIDTH,
+            height: HEIGHT,
+            title: "Cellular Automata".to_string(),
+            present_mode: bevy::window::PresentMode::Immediate,
+            resizable: true,
+            mode: WindowMode::Windowed,
+            ..WindowDescriptor::default()
+        })
+        // Add needed plugins
+        .add_plugin(bevy::core::CorePlugin)
+        .add_plugin(bevy::log::LogPlugin)
+        .add_plugin(bevy::time::TimePlugin)
+        .add_plugin(bevy::diagnostic::LogDiagnosticsPlugin::default())
+        .add_plugin(bevy::diagnostic::DiagnosticsPlugin)
+        .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
+        .add_plugin(bevy::input::InputPlugin)
+        .add_plugin(VulkanoWinitPlugin)
+        .add_startup_system(setup)
+        .add_system(close_on_esc)
+        .add_system(input_actions)
+        .add_system(update_camera)
+        .add_system(update_mouse)
+        .add_system(draw_matter)
+        // Simulate only SIM_FPS times per second. Instead of add_system(simulate)
+        .add_system_set_to_stage(
+            CoreStage::Update,
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::steps_per_second(SIM_FPS))
+                .with_system(simulate),
+        )
+        .add_system(simulate)
+        // Gui
+        .add_system(user_interface)
+        // Render after update
+        .add_system_to_stage(CoreStage::PostUpdate, render)
+        .run();
+}
+
+/// Creates our simulation & render pipelines
+fn setup(mut commands: Commands, vulkano_windows: NonSend<BevyVulkanoWindows>) {
+    let (primary_window_renderer, _gui) = vulkano_windows.get_primary_window_renderer().unwrap();
+    // Create our render pass
+    let fill_screen = FillScreenRenderPass::new(
+        primary_window_renderer.graphics_queue(),
+        primary_window_renderer.swapchain_format(),
+    );
+    let simulator = CASimulator::new(primary_window_renderer.compute_queue());
+
+    // Create simple orthographic camera
+    let mut camera = OrthographicCamera::default();
+    // Zoom camera to fit vertical pixels
+    camera.zoom_to_fit_vertical_pixels(CANVAS_SIZE_Y, HEIGHT as u32);
+    // Insert resources
+    commands.insert_resource(fill_screen);
+    commands.insert_resource(camera);
+    commands.insert_resource(simulator);
+    commands.insert_resource(PreviousMousePos(None));
+    commands.insert_resource(CurrentMousePos(None));
+    commands.insert_resource(DynamicSettings::default());
+}
+
+/// Step simulation
+fn simulate(mut sim_pipeline: ResMut<CASimulator>, settings: Res<DynamicSettings>) {
+    sim_pipeline.step(1, settings.is_paused);
+}
+
+/// Render the simulation
+fn render(
+    mut vulkano_windows: NonSendMut<BevyVulkanoWindows>,
+    mut fill_screen: ResMut<FillScreenRenderPass>,
+    camera: Res<OrthographicCamera>,
+    simulator: Res<CASimulator>,
+) {
+    let (window_renderer, gui) = vulkano_windows.get_primary_window_renderer_mut().unwrap();
+    // Start frame
+    let before = match window_renderer.acquire() {
+        Err(e) => {
+            bevy::log::error!("Failed to start frame: {}", e);
+            return;
+        }
+        Ok(f) => f,
     };
-    eframe::run_native(
-        "Team Lukas app",
-        options,
-        Box::new(|_cc| {
-            Box::<MyApp>::default()
-        }),
-    )
+
+    let canvas_image = simulator.color_image();
+
+    // Render
+    let final_image = window_renderer.swapchain_image_view();
+    let after_images = fill_screen.draw(
+        before,
+        *camera,
+        canvas_image,
+        final_image.clone(),
+        CLEAR_COLOR,
+        false,
+        true,
+    );
+    // Draw gui
+    let after_gui = gui.draw_on_image(after_images, final_image);
+    // Finish Frame
+    window_renderer.present(after_gui, true);
+}
+
+/// Update camera (if window is resized)
+fn update_camera(windows: Res<Windows>, mut camera: ResMut<OrthographicCamera>) {
+    let window = windows.get_primary().unwrap();
+    camera.update(window.width(), window.height());
+}
+
+/// Input actions for camera movement, zoom and pausing
+fn input_actions(
+    time: Res<Time>,
+    mut camera: ResMut<OrthographicCamera>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut mouse_input_events: EventReader<MouseWheel>,
+) {
+    // Move camera with arrows & WASD
+    let up = keyboard_input.pressed(KeyCode::W) || keyboard_input.pressed(KeyCode::Up);
+    let down = keyboard_input.pressed(KeyCode::S) || keyboard_input.pressed(KeyCode::Down);
+    let left = keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::Left);
+    let right = keyboard_input.pressed(KeyCode::D) || keyboard_input.pressed(KeyCode::Right);
+
+    let x_axis = -(right as i8) + left as i8;
+    let y_axis = -(up as i8) + down as i8;
+
+    let mut move_delta = Vec2::new(x_axis as f32, y_axis as f32);
+    if move_delta != Vec2::ZERO {
+        move_delta /= move_delta.length();
+        camera.pos += move_delta * time.delta_seconds() * CAMERA_MOVE_SPEED;
+    }
+
+    // Zoom camera with mouse scroll
+    for e in mouse_input_events.iter() {
+        if e.y < 0.0 {
+            camera.scale *= 1.05;
+        } else {
+            camera.scale *= 1.0 / 1.05;
+        }
+    }
+}
+
+/// Draw matter to our grid
+fn draw_matter(
+    mut simulator: ResMut<CASimulator>,
+    prev: Res<PreviousMousePos>,
+    current: Res<CurrentMousePos>,
+    mouse_button_input: Res<Input<MouseButton>>,
+    settings: Res<DynamicSettings>,
+) {
+    if let Some(current) = current.0 {
+        if mouse_button_input.pressed(MouseButton::Left) {
+            let line = get_canvas_line(prev.0, current);
+            simulator.draw_matter(&line, settings.brush_radius, settings.draw_matter);
+        }
+    }
+}
+
+/// Mouse position from last frame
+#[derive(Debug, Copy, Clone)]
+pub struct PreviousMousePos(pub Option<MousePos>);
+
+/// Mouse position now
+#[derive(Debug, Copy, Clone)]
+pub struct CurrentMousePos(pub Option<MousePos>);
+
+/// Update mouse position
+fn update_mouse(
+    windows: Res<Windows>,
+    mut _prev: ResMut<PreviousMousePos>,
+    mut _current: ResMut<CurrentMousePos>,
+    camera: Res<OrthographicCamera>,
+) {
+    _prev.0 = _current.0;
+    let primary = windows.get_primary().unwrap();
+    if primary.cursor_position().is_some() {
+        _current.0 = Some(MousePos {
+            world: cursor_to_world(primary, camera.pos, camera.scale),
+        });
+    }
 }
---
+++ src/matter.rs
@@ -0,0 +1,92 @@
+use rand::Rng;
+use strum_macros::EnumIter;
+
+use crate::{
+    utils::{u32_rgba_to_u8_rgba, u8_rgba_to_u32_rgba},
+    EMPTY_COLOR,
+};
+
+/// Matter Id representing matter that we simulate
+#[repr(u8)]
+#[derive(EnumIter, Debug, Copy, Clone, Eq, PartialEq)]
+pub enum MatterId {
+    Empty = 0,
+    Sand = 1,
+    Stone = 2,
+}
+
+impl Default for MatterId {
+    fn default() -> Self {
+        MatterId::Empty
+    }
+}
+
+impl From<u8> for MatterId {
+    fn from(item: u8) -> Self {
+        unsafe { std::mem::transmute(item) }
+    }
+}
+
+impl MatterId {
+    fn color_rgba_u8(&self) -> [u8; 4] {
+        let color = match *self {
+            MatterId::Empty => EMPTY_COLOR,
+            MatterId::Sand => 0xc2b280ff,
+            MatterId::Stone => 0x6B6867ff,
+        };
+        u32_rgba_to_u8_rgba(color)
+    }
+
+    fn gen_variate_color_rgba_u8(&self) -> [u8; 4] {
+        let p = rand::thread_rng().gen::<f32>();
+        let color = self.color_rgba_f32();
+        let variation = -0.1 + 0.2 * p;
+        let r = ((color[0] + variation).clamp(0.0, 1.0) * 255.0) as u8;
+        let g = ((color[1] + variation).clamp(0.0, 1.0) * 255.0) as u8;
+        let b = ((color[2] + variation).clamp(0.0, 1.0) * 255.0) as u8;
+        let a = 255;
+        [r, g, b, a]
+    }
+
+    fn color_rgba_f32(&self) -> [f32; 4] {
+        let rgba = self.color_rgba_u8();
+        [
+            rgba[0] as f32 / 255.0,
+            rgba[1] as f32 / 255.0,
+            rgba[2] as f32 / 255.0,
+            rgba[3] as f32 / 255.0,
+        ]
+    }
+}
+
+/// Matter data where first 3 bytes are saved for color and last 4th byte is saved for matter id
+#[derive(Default, Copy, Clone)]
+pub struct MatterWithColor {
+    pub value: u32,
+}
+
+impl MatterWithColor {
+    /// Creates a new matter with color from matter id giving it a slightly randomized color
+    pub fn new(matter_id: MatterId) -> MatterWithColor {
+        let color = if matter_id != MatterId::Empty {
+            matter_id.gen_variate_color_rgba_u8()
+        } else {
+            matter_id.color_rgba_u8()
+        };
+        MatterWithColor {
+            value: u8_rgba_to_u32_rgba(color[0], color[1], color[2], matter_id as u8),
+        }
+    }
+
+    pub fn matter_id(&self) -> MatterId {
+        ((self.value & 255) as u8).into()
+    }
+}
+
+impl From<u32> for MatterWithColor {
+    fn from(item: u32) -> Self {
+        Self {
+            value: item,
+        }
+    }
+}
